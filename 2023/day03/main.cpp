a9aa0b1bfdb67d7099483c78fda9095533c76696
Slave <theotirel@gmail.com>
2024-10-06 08:08:35 +0200

    day3 part 2 completed

--- 2023/day03/main.cpp
+++ 2023/day03/main.cpp
@@ -19,7 +19,7 @@ using std::map;
 using std::pair;
 
 using ptr_matrix=shared_ptr<vector<vector<char>>>;
-using ptr_map=shared_ptr<map<pair<unsigned int,unsigned int>,unsigned int>>;
+using ptr_map=shared_ptr<map<pair<unsigned int,unsigned int>,pair<unsigned int,unsigned int>>>;
 
 ptr_matrix create_matrix(std::ifstream& file){
     string line;
@@ -41,11 +41,19 @@ ptr_matrix create_matrix(std::ifstream& file){
     return matrix_ptr;
 }
 
-bool check_arround(ptr_matrix& matrix_ptr, int i, int j, int k,int number_lines,int length){
+   
+bool check_arround(ptr_matrix& matrix_ptr,ptr_map& map_ptr, int i, int j, int k,int number_lines,int length,int n){
     int i_min = max(i-1,0),i_max = min(i+1,number_lines-1),j_min = max(j-1,0),j_max = min(k+1,length-1);
     for(int o = i_min;o<=i_max;++o){
         for(int p = j_min;p<=j_max;++p){
             const char& c = (*matrix_ptr)[o][p];
+            if(c == '*'){
+                auto ret = map_ptr->insert({{o,p},{1,n}});
+                if(!ret.second){
+                    ++ret.first->second.first;
+                    ret.first->second.second*=n;
+                }
+            }
             if(!isdigit(c) && c != '.' ){
                 return true;
             }
@@ -57,8 +65,8 @@ bool check_arround(ptr_matrix& matrix_ptr, int i, int j, int k,int number_lines,
 int main(){
     std::ifstream file("input");
     ptr_matrix matrix_ptr = create_matrix(file);
-    ptr_map map_ptr = make_shared<map<pair<unsigned int,unsigned int>,unsigned int>>();
-    unsigned int sum = 0,j,k,n=0,length=(*matrix_ptr)[0].size(),number_lines=matrix_ptr->size();
+    ptr_map map_ptr = make_shared<map<pair<unsigned int,unsigned int>,pair<unsigned int,unsigned int>>>();
+    unsigned int sum = 0,sum_gear=0,j,k,n=0,length=(*matrix_ptr)[0].size(),number_lines=matrix_ptr->size();
     for(unsigned int i=0;i<number_lines;++i){
         j=0;
         vector<char>& line = (*matrix_ptr)[i];
@@ -73,7 +81,7 @@ int main(){
                 for( unsigned int l = j;l<k;++l){
                     n += (line[l]-'0')*pow(10,k-l-1);
                 }
-                if(check_arround(matrix_ptr, i, j, k-1,number_lines,length)){
+                if(check_arround(matrix_ptr,map_ptr, i, j, k-1,number_lines,length,n)){
                     sum += n;
                 }
                 j=k+1;
@@ -82,7 +90,13 @@ int main(){
             }
         }
     }
+    for(auto beg = map_ptr->begin();beg!=map_ptr->end();++beg){
+        if(beg->second.first==2){
+            sum_gear += beg->second.second;
+        }
+    }
     file.close();
     cout<< "The sum of the part numbers in the engine schematic is: "<< sum << endl; 
+    cout<< "The sum of all of the gear ratios is : "<< sum_gear << endl; 
     return 0;
 }

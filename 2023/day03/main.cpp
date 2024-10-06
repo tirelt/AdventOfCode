8cc78626228325183717c684a4650888f1cdf48c
Slave <theotirel@gmail.com>
2024-10-06 04:34:48 +0200

    day 3 part 1 completed

--- 2023/day03/main.cpp
+++ 2023/day03/main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <memory>
 #include <math.h>
+#include <map>
+#include <utility>
 
 using std::string;
 using std::cout;
@@ -13,64 +15,67 @@ using std::shared_ptr;
 using std::make_shared;
 using std::max;
 using std::min;
+using std::map;
+using std::pair;
 
-using ptr_type=shared_ptr<vector<vector<const char>>>;
+using ptr_matrix=shared_ptr<vector<vector<char>>>;
+using ptr_map=shared_ptr<map<pair<unsigned int,unsigned int>,unsigned int>>;
 
-ptr_type create_matrix(std::ifstream& file){
+ptr_matrix create_matrix(std::ifstream& file){
     string line;
-    auto matrix_ptr = make_shared<vector<vector<const char>>>();
-    vector<const char> vect_line;
+    ptr_matrix matrix_ptr = make_shared<vector<vector<char>>>();
+    vector<char> vect_line;
     int length=-1;
     while(getline(file,line)){
-        if(line.size()!=length && length ==-1){
+        if(line.size()!=length && length !=-1){
             throw std::runtime_error("Lines don't have the same length");
         } else {
             length = line.size();
         }
-        for(const char& c : line){
+        vect_line.clear();
+        for(const char c : line){
             vect_line.push_back(c);
         }
         matrix_ptr->push_back(vect_line);
     }
     return matrix_ptr;
 }
-bool check_arround(ptr_type matrix_ptr, int i, int j, int k){
-    int length=(*matrix_ptr)[0].size(),number_lines = matrix_ptr->size();
+
+bool check_arround(ptr_matrix& matrix_ptr, int i, int j, int k,int number_lines,int length){
     int i_min = max(i-1,0),i_max = min(i+1,number_lines-1),j_min = max(j-1,0),j_max = min(k+1,length-1);
-    bool include = false;
     for(int o = i_min;o<=i_max;++o){
         for(int p = j_min;p<=j_max;++p){
-            const char& c = (*matrix_ptr)[i][j];
-            if(!isalpha(c) && c != '.' ){
-                include = true;
-                break;
+            const char& c = (*matrix_ptr)[o][p];
+            if(!isdigit(c) && c != '.' ){
+                return true;
             }
         }
-        if(include){
-            break;
-        }
     }
+    return false;
 }
 
 int main(){
-    std::ifstream file("test_input");
-    ptr_type matrix_ptr = create_matrix(file);
-    unsigned int sum = 0,j,k,n,length=(*matrix_ptr)[0].size();
-    vector<const char> line;
-    for(unsigned int i=0;i<matrix_ptr->size();++i){
+    std::ifstream file("input");
+    ptr_matrix matrix_ptr = create_matrix(file);
+    ptr_map map_ptr = make_shared<map<pair<unsigned int,unsigned int>,unsigned int>>();
+    unsigned int sum = 0,j,k,n=0,length=(*matrix_ptr)[0].size(),number_lines=matrix_ptr->size();
+    for(unsigned int i=0;i<number_lines;++i){
         j=0;
-        line = (*matrix_ptr)[i];
+        vector<char>& line = (*matrix_ptr)[i];
         while(j<length){
-            const char c = line[j];
+            const char& c = line[j];
             if(isdigit(c)){
                 k=j+1;
                 while(k<length && isdigit(line[k])){
                     ++k;
                 }
+                n=0;
                 for( unsigned int l = j;l<k;++l){
                     n += (line[l]-'0')*pow(10,k-l-1);
                 }
-
+                if(check_arround(matrix_ptr, i, j, k-1,number_lines,length)){
+                    sum += n;
+                }
                 j=k+1;
             } else{
                 ++j;

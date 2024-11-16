da9a06ad64de0c248cb29f59154d5fb4216b9aaa
Mac theo <theotirel@gmail.com>
2024-11-16 17:28:34 +0800

    day12 part2 completed

--- 2023/day12/main.cpp
+++ 2023/day12/main.cpp
@@ -4,12 +4,7 @@
 #include <vector>
 #include <deque>
 #include <string>
-#include <algorithm>
 #include <map>
-#include <cmath>
-#include <numeric>
-#include <map>
-#include <tuple>
 
 using std::cout;
 using std::endl;
@@ -17,24 +12,18 @@ using std::vector;
 using std::deque;
 using std::string;
 using std::istringstream;
-using std::accumulate;
-using std::copy;
 using std::map;
-using std::tuple;
 
-void process_line(string& line, vector<char>& status, vector<unsigned>& damaged_group,deque<vector<char>>& to_insert,map<unsigned,char>& status_to_check){
+void process_line(string& line, vector<char>& status,deque<vector<char>>& to_insert){
     bool has_seen_separator = false;
     status.clear();
-    damaged_group.clear();
     to_insert.clear();
-    status_to_check.clear();
     unsigned sized = 0;
     vector<char> temp;
     for(unsigned i=0;i<5;++i){
         auto ite = line.begin();
         while( *ite != ' ' ){
             status.push_back(*ite);
-            if(*ite!='?') status_to_check.insert({ite-line.begin()+sized,*ite});
             ++ite;
         }
         status.push_back('?');
@@ -44,8 +33,7 @@ void process_line(string& line, vector<char>& status, vector<unsigned>& damaged_
         string sub_line(ite,line.end()),word;
         istringstream record(sub_line);
         while(getline(record,word,',')){
-            damaged_group.push_back(stoi(word));
-            temp = vector<char>(damaged_group.back(),'#');
+            temp = vector<char>(stoi(word),'#');
             temp.push_back('.');
             to_insert.push_back(temp);
         }
@@ -53,55 +41,6 @@ void process_line(string& line, vector<char>& status, vector<unsigned>& damaged_
     status.pop_back();
     to_insert.back().pop_back();
 }
-
-unsigned long long fact(unsigned n){
-    if( n ==0 || n == 1 ){
-        return 1;
-    } else {
-        return n *fact(n-1);
-    }
-}
-
-unsigned long long combin(unsigned n, unsigned k){
-    return fact(n)/(fact(k)*fact(n-k));
-}
-unsigned number_of_possibilities(vector<char>& status, vector<unsigned>& damaged_group){
-    unsigned n = (status.size()-(accumulate(damaged_group.begin(),damaged_group.end(),0)+damaged_group.size()-1)) + damaged_group.size();
-    unsigned k = damaged_group.size();
-    return combin(n, k);
-}
-
-bool check_current_status(vector<char>& current_status,map<unsigned,char>& status_to_check){
-    auto ite = status_to_check.begin();
-    while(ite != status_to_check.end() && ite->first < current_status.size() && current_status[ite->first]==ite->second){
-        ++ite;
-    }
-    return ite == status_to_check.end() || ite->first >= current_status.size();
-}
-unsigned count_possibilities(deque<vector<char>> to_insert,vector<char> current_status,unsigned number_of_extra_status, map<unsigned,char>& status_to_check, unsigned& call_counter){
-    call_counter += 1;
-    if(to_insert.size()>0){
-        unsigned sum = 0;
-        for(unsigned i = 0;i<=number_of_extra_status;++i){
-            deque<vector<char>> to_insert_bis = to_insert;
-            vector<char> current_status_bis = current_status;
-            for(unsigned j=0;j<i;++j) current_status_bis.push_back('.');
-            copy(to_insert_bis.front().begin(),to_insert_bis.front().end(),back_inserter(current_status_bis));
-            to_insert_bis.pop_front();
-            if(check_current_status(current_status_bis,status_to_check)){
-                sum += count_possibilities(to_insert_bis,current_status_bis,number_of_extra_status-i, status_to_check,call_counter);
-            }
-        }    
-        return sum;
-    } else {
-        if(number_of_extra_status){
-            for(unsigned j=0;j<number_of_extra_status;++j) current_status.push_back('.');
-        }
-        return check_current_status(current_status,status_to_check);
-    }
-}
-
-using key_type = tuple<deque<vector<char>>::iterator,deque<vector<char>>::iterator,vector<char>::iterator,vector<char>::iterator> ;
 
 bool check_can_insert(vector<char>& to_insert,vector<char>::iterator& status_start){
     bool ret = true;
@@ -151,17 +90,13 @@ int main(){
     vector<unsigned> damaged_group;
     deque<vector<char>> to_insert;
     map<unsigned,char> status_to_check;
-    unsigned call_counter=0,number_of_extra_status,ret, sum = 0,counter = 0;
+    long long unsigned sum=0,ret;
     map<unsigned,long long unsigned> memory;
     while(getline(file,line)){
-        call_counter = 0;
-        process_line(line,status,damaged_group,to_insert,status_to_check);
-        number_of_extra_status = (status.size()-(accumulate(damaged_group.begin(),damaged_group.end(),0)+damaged_group.size()-1));
-        //ret = count_possibilities(to_insert,{},number_of_extra_status,status_to_check, call_counter);
+        process_line(line,status,to_insert);
         memory.clear();
         ret = count_possibilities_smart(to_insert.begin(),to_insert.end(),status.begin(), status.end(),memory);
-        //cout << ++counter << endl;
-        output << number_of_possibilities(status, damaged_group)  << " " << memory.size()<< " "<< ret << endl;
+        output << memory.size()<< " "<< ret << endl;
         sum += ret;
     }
     file.close();

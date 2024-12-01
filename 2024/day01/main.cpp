78be7e4132c129ce16f9a2ce6794c9e6757465e1
Mac theo <theotirel@gmail.com>
2024-12-02 03:59:49 +0800

    clean up

--- 2024/day01/main.cpp
+++ 2024/day01/main.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <chrono>
 #include <map>
 
 using std::cout;
@@ -14,7 +13,6 @@ using std::map;
 using std::sort;
 
 int main(){
-    auto start = std::chrono::high_resolution_clock::now();
     std::ifstream file("input");
     string line;
     vector<int> list_1;
@@ -23,8 +21,6 @@ int main(){
     while(getline(file,line)){
         list_1.push_back(stoi(line.substr(0,5))); 
         list_2.push_back(stoi(line.substr(8,5))); 
-        //list_1.push_back(stoi(line.substr(0,1))); 
-        //list_2.push_back(stoi(line.substr(4,1)));
         count_map[list_2.back()] += 1;
     }
     file.close();
@@ -41,8 +37,5 @@ int main(){
         res_2 += i*count_map[i];
     }
     cout<< "The similarity score is: " << res_2 << endl;  
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    std::cout << "Execution time: " << duration.count() << " seconds" << std::endl;
     return 0;
 }

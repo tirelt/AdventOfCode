07be9ffe1dcd6e0fd521520fb2e025447e4b21c8
Mac theo <theotirel@gmail.com>
2025-01-02 23:05:03 +0800

    23day18 part2 progress, need to debug

--- 2023/day18/main.cpp
+++ 2023/day18/main.cpp
@@ -61,34 +61,70 @@ int depth_search(pair<int,int> pos_init, vector<vector<char>>& plan){
     return number;
 }
 
-bool collaspe(list<pair<char,int>>::iterator& ite,list<pair<char,int>>::iterator& new_ite,list<pair<char,int>>& instructions_2){
+template<typename T> 
+struct CircularList{
+    using Iterator = typename std::list<T>::iterator;
+    CircularList() = default;
+    std::list<T> elements;
+    void push_back(const T& value){ elements.push_back(value); return; }
+    size_t size(){ return elements.size(); }
+    Iterator begin() { return elements.begin(); }
+    Iterator end() { return elements.end(); }
+    Iterator next(Iterator it);
+    Iterator prev(Iterator it);
+    Iterator erase(Iterator ite){ 
+        auto ret = elements.erase(ite);
+        if (ret == elements.end())
+            ret = elements.begin();
+        return ret;
+    }
+};
+
+template<typename T> 
+typename CircularList<T>::Iterator CircularList<T>::next(typename CircularList<T>::Iterator it){
+    ++it;
+    if (it == elements.end())
+        it = elements.begin();
+    return it;
+}
+
+template<typename T> 
+typename CircularList<T>::Iterator CircularList<T>::prev(typename CircularList<T>::Iterator it){
+    if (it == elements.begin())
+        return --elements.end();
+    else 
+        return --it;
+    return it;
+}
+
+template<typename T> 
+typename CircularList<T>::Iterator collaspe(typename CircularList<T>::Iterator ite,CircularList<T>& instructions_2){
+    auto new_ite = instructions_2.prev(ite);
     if(new_ite->first == ite->first){
         ite->second += new_ite->second;
-        instructions_2.erase(new_ite);
-    } else if((new_ite->first = 'R' && ite->first=='L') || (new_ite->first = 'L' && ite->first=='R') && (new_ite->first = 'U' && ite->first=='D') || (new_ite->first = 'D' && ite->first=='U')){
+        ite = instructions_2.erase(new_ite);
+    } else if((new_ite->first == 'R' && ite->first=='L') || (new_ite->first == 'L' && ite->first=='R') || (new_ite->first == 'U' && ite->first=='D') || (new_ite->first == 'D' && ite->first=='U')){
         if(ite->second>new_ite->second){
             ite->second -= new_ite->second;
-            instructions_2.erase(new_ite);
+            ite = instructions_2.erase(new_ite);
         } else if(new_ite->second>ite->second){
             new_ite->second -= ite->second;
-            instructions_2.erase(ite);
-            ite = new_ite;
+            ite = instructions_2.erase(ite);
         } else{
-            instructions_2.erase(new_ite);
-            instructions_2.erase(ite++);
+            ite = instructions_2.erase(new_ite);
+            ite = instructions_2.erase(ite);
         }
-    } else{
-        return false;
     }
-    return true;
+    return ite;
 }
+
 int main(){
     std::ifstream file("test_input");
     regex pattern(R"((\w)\s(\d+)\s\(#([a-fA-F0-9]+)\))");
     smatch matches;
     string line;
     list<pair<char,int>> instructions_1;
-    list<pair<char,int>> instructions_2;
+    CircularList<pair<char,long long>> instructions_2;
     int i=0,j=0,max_i=0,min_i=0,max_j=0,min_j=0;
     while(getline(file,line)){
         regex_search(line,matches,pattern);
@@ -191,9 +227,8 @@ int main(){
     set<string> recognized_pattern{"URD","RDL","DLU","LUR"};
     auto ite = instructions_2.begin();
     while(instructions_2.size()>2 && index<max_ite){
-        auto prev_ite = (ite == instructions_2.begin()) ? instructions_2.end() : prev(ite);--prev_ite;
-        auto next_ite = ite;++next_ite;
-        if(next_ite==instructions_2.end()) next_ite = instructions_2.begin();
+        auto prev_ite = instructions_2.prev(ite);
+        auto next_ite = instructions_2.next(ite);
         string p;
         p.push_back(prev_ite->first);
         p.push_back(ite->first);
@@ -202,26 +237,19 @@ int main(){
             if(prev_ite->second>next_ite->second){
                 prev_ite->second -= next_ite->second;
                 ret_2 += next_ite->second*ite->second;
-                instructions_2.erase(next_ite);
+                ite = instructions_2.erase(next_ite);
+                ite = instructions_2.prev(ite);
             } else if (next_ite->second>prev_ite->second){
                 next_ite->second -= prev_ite->second;
                 ret_2 += prev_ite->second*ite->second;
-                instructions_2.erase(prev_ite);
+                ite = instructions_2.erase(prev_ite);
             } else{
                 ret_2 += prev_ite->second*ite->second;
-                instructions_2.erase(prev_ite);
-                instructions_2.erase(next_ite);
+                ite = instructions_2.erase(prev_ite);
+                ite = instructions_2.prev(instructions_2.erase(instructions_2.next(ite)));
             }
-            auto new_ite = (ite == instructions_2.begin()) ? instructions_2.end() : prev(ite);--new_ite;
-            while(collaspe(ite,new_ite,instructions_2)){
-                new_ite = (ite == instructions_2.begin()) ? instructions_2.end() : prev(ite);--new_ite;
-            }
-            new_ite = ite;++next_ite;
-            if(next_ite==instructions_2.end()) next_ite = instructions_2.begin();
-            while(collaspe(ite,new_ite,instructions_2)){
-                new_ite = ite;++next_ite;
-                if(next_ite==instructions_2.end()) next_ite = instructions_2.begin();
-            }
+            ite = collaspe(ite,instructions_2);
+            ite = collaspe(instructions_2.next(ite),instructions_2);
         } else{
             ++ite;
         }

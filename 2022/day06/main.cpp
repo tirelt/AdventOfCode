fec25d4c86c37dcec528c9b3f0a987f4ff273381
Mac theo <theotirel@gmail.com>
2025-06-16 21:57:20 +0800

    2022day06 part2

--- 2022/day06/main.cpp
+++ 2022/day06/main.cpp
@@ -11,22 +11,28 @@ using std::endl;
 using std::set;
 using std::deque;
 
+int aux(const string& line,int n){
+    int res_1 = 0;
+    deque<char> last_four;
+    for(const char& c : line){
+        ++res_1;
+        last_four.push_back(c);
+        if(last_four.size()>=n){
+            set<char> seen(last_four.begin(), last_four.end());
+            if(seen.size() == n)
+                break;
+            last_four.pop_front();
+        }        
+    }
+    return res_1;
+}
 int main(){
     std::ifstream file("input");
     string line;
     int res_1 = 0, res_2 = 0;
-    deque<char> last_four;
     while(getline(file,line)){
-        for(const char& c : line){
-            ++res_1;
-            last_four.push_back(c);
-            if(last_four.size()>=4){
-                set<char> seen(last_four.begin(), last_four.end());
-                if(seen.size() == 4)
-                    break;
-                last_four.pop_front();
-            }        
-        }
+        res_1 += aux(line,4);
+        res_2 += aux(line,14);
     }
     file.close();
 

b85d07e388f95125d59e22be0959015e3cdc0810
Mac theo <theotirel@gmail.com>
2025-06-23 19:58:02 +0800

    2022day08 preparing

---
+++ 2022/day09/main.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main(){
+    std::ifstream file("test_input");
+    string line;
+    while(getline(file,line)){
+    }
+    cout<< "Part 1: " << 0 << endl; 
+    cout<< "Part 2: " << 0 << endl; 
+    return 0;
+}

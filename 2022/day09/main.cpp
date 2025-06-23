f2bbc287f7fe1aa0fc3983542c3281cecf82362b
Mac theo <theotirel@gmail.com>
2025-06-23 21:03:32 +0800

    2022day09 part2 just refactor a bit part1

--- 2022/day09/main.cpp
+++ 2022/day09/main.cpp
@@ -12,35 +12,49 @@ using std::vector;
 using std::pair;
 using std::set;
 
-int main(){
-    std::ifstream file("input");
-    string line;
-    pair<int,int> head(0,0), tail(0,0);
+int process(const vector<string>& lines, vector<pair<int,int>> knots) {
     set<pair<int,int>> visited;
-    visited.insert(tail); 
-    while(getline(file,line)){
+    visited.insert({0, 0});
+    for(const string& line : lines) {
         char direction = line[0];
         int steps = std::stoi(line.substr(2));
-        for(int i = 0; i < steps; ++i){
-            switch(direction){
+        pair<int,int>& head = knots.front();
+        for(int i = 0; i < steps; ++i) {
+            switch(direction) {
                 case 'U': ++head.second; break;
                 case 'D': --head.second; break;
                 case 'L': --head.first; break;
                 case 'R': ++head.first; break;
             }
-            if(abs(head.first - tail.first) > 1 || abs(head.second - tail.second) > 1){
-                if(head.first != tail.first) {
-                    tail.first += (head.first > tail.first) ? 1 : -1;
-                }
-                if(head.second != tail.second) {
-                    tail.second += (head.second > tail.second) ? 1 : -1;
+            for(int j = 1; j < knots.size(); ++j) {
+                pair<int,int>& tail = knots[j];
+                pair<int,int>& head = knots[j-1];
+                if(abs(head.first - tail.first) > 1 || abs(head.second - tail.second) > 1) {
+                    if(head.first != tail.first) {
+                        tail.first += (head.first > tail.first) ? 1 : -1;
+                    }
+                    if(head.second != tail.second) {
+                        tail.second += (head.second > tail.second) ? 1 : -1;
+                    }
                 }
             }
-            visited.insert(tail);
+            visited.insert(knots.back());
         }
-
     }
-    cout<< "Part 1: " << visited.size() << endl; 
-    cout<< "Part 2: " << 0 << endl; 
+    return visited.size();
+}
+
+int main(){
+    std::ifstream file("input");
+    string line;
+    vector<pair<int,int>> knots{2, pair<int,int>(0,0)};
+    set<pair<int,int>> visited;
+    visited.insert({0, 0}); 
+    vector<string> lines;
+    while(getline(file,line)){
+        lines.push_back(line);
+    }
+    cout<< "Part 1: " << process(lines,{2, pair<int,int>(0,0)}) << endl; 
+    cout<< "Part 2: " << process(lines,{10, pair<int,int>(0,0)}) << endl; 
     return 0;
 }

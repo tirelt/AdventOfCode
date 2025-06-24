392959e2a97090ce3d88c72d5447b0c051575cfe
Mac theo <theotirel@gmail.com>
2025-06-24 22:25:27 +0800

    2022day10 part2

--- 2022/day10/main.cpp
+++ 2022/day10/main.cpp
@@ -17,14 +17,15 @@ int main(){
     std::ifstream file("input");
     string line;
     int res_1 = 0, x  = 1, cycle = 0;
-    vector<int> interesting_cycles = {20, 60, 100, 140, 180, 220};
+    vector<int> interesting_cycles = {20, 60, 100, 140, 180, 220,260};
     int interesting_cycle_index = 0;
+    vector<pair<int, int>> positions; 
     while(getline(file,line)){
         if(line.substr(0, 4) == "noop")
             ++cycle;
         else
             cycle += 2; 
-
+        positions.push_back({cycle, x});
         if(cycle >= interesting_cycles[interesting_cycle_index] ){
             res_1 += x * interesting_cycles[interesting_cycle_index];
             ++interesting_cycle_index;
@@ -35,7 +36,21 @@ int main(){
         if(line.substr(0, 4) == "addx")
             x += std::stoi(line.substr(5));
     }
+    positions.push_back({241, x});
     cout<< "Part 1: " << res_1 << endl; 
-    cout<< "Part 2: " << 0 << endl; 
+
+    cout<< "Part 2: "<< endl; 
+    int index = 0;
+    x = 1; 
+    for(int i = 1; i <= 240; ++i){
+        if((i-1)%40 == 0) cout << "\n";
+        if( i > positions[index].first) 
+            x = positions[++index].second;
+        if(((i-1)%40+1) >= x && ((i-1)%40+1) <= x + 2)
+            cout << "#";
+        else
+            cout << ".";
+    }
+    cout << endl;
     return 0;
 }

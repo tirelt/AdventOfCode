4b5051a40f707b9892a34fadd511f957e3de1337
Mac theo <theotirel@gmail.com>
2025-07-06 11:55:49 +0800

    2022day14 part2 same than part 1, too lazy to make it nice

--- 2022/day14/main.cpp
+++ 2022/day14/main.cpp
@@ -93,7 +93,68 @@ int main(){
     cout<< "Part 1: " << ret_1 << endl; 
     
     // Part 2
-    cout<< "Part 2: " << 0 << endl; 
+    max_y += 2;
+    min_x = std::min(min_x, 500 - max_y);
+    max_x = std::max(max_x, 500 + max_y);
+    vector<vector<char>> grid_2(max_y-min_y+1, vector<char>(max_x - min_x + 1, '.'));
+    grid = grid_2; 
+    for (const auto& rock : rocks) {
+        for (int i = 0; i < rock.size() - 1; ++i) {
+            grid_2[rock[i].second - min_y][rock[i].first - min_x] = '#';
+            if(i+1 < rock.size()) {
+                int x1 = rock[i].first - min_x;
+                int y1 = rock[i].second - min_y;
+                int x2 = rock[i + 1].first - min_x;
+                int y2 = rock[i + 1].second - min_y;
+                if (x1 == x2) { // vertical line
+                    for (int j = std::min(y1, y2); j <= std::max(y1, y2); ++j) {
+                        grid[j][x1] = '#';
+                    }
+                } else if (y1 == y2) { // horizontal line
+                    for (int j = std::min(x1, x2); j <= std::max(x1, x2); ++j) {
+                        grid[y1][j] = '#';
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < grid[0].size(); ++i) {
+        grid[max_y - min_y][i] = '#'; // Add the floor
+    }
+    int ret_2 = 0;
+    while (true) {
+        int x = 500 - min_x;
+        int y = 0;
+        bool escaped = false;
+        while (y < grid.size() ) {
+            if (y+1 == grid.size()){
+                escaped = true; 
+                break;
+            } else if (grid[y + 1][x] == '.') {
+                y++;
+            } else if( x == 0 ){
+                escaped = true;
+                break;
+            } else if (grid[y + 1][x - 1] == '.') {
+                y++;
+                x--;
+            } else if (x == grid[0].size() - 1) {
+                escaped = true;
+                break;
+            } else if (grid[y + 1][x + 1] == '.') {
+                y++;
+                x++;
+            } else {
+                break;
+            }
+        }
+        if (escaped || (y == 0 && x == 500 - min_x)) {
+            break; 
+        }
+        grid[y][x] = 'o';
+        ret_2++;
+    }
+    cout<< "Part 2: " << ret_2+1 << endl; 
 
     return 0;
 }

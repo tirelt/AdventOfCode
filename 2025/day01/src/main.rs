9cd842a977876554b37ae6ea0f63ab1a83db8435
Mac theo <theotirel@gmail.com>
2025-12-01 21:37:32 +0800

    2025part1

--- 2025/day01/src/main.rs
+++ 2025/day01/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("input").expect("should work");
+    let mut pos = 50;
+    let mut res = 0;
+    for line in file.lines(){
+        let instructions = line.trim();
+        let mut ite = instructions.chars();
+        let dir = ite.next().unwrap();
+        let rest: String = ite.collect();
+        let n: i32 = rest.parse().unwrap();
+        if dir == 'L' { 
+            pos = (pos + 100 - n) % 100;
+        } else {
+            pos = (pos + n ) % 100;
+        }
+        if pos == 0{
+            res += 1;
+        }
+    }
+    println!("Part 1: {res}");
 }

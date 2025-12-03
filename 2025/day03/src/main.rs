0825d40e96b1945b7b9bf1730050b9ed31dc6b2c
theo <theotirel@gmail.com>
2025-12-03 22:54:22 +0800

    clean up

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -1,34 +1,11 @@
-use std::collections::HashMap;
 use std::fs;
-
-fn part_1(line: &str) -> i32 {
-    let mut ite = line.chars().peekable();
-    let mut max = ite.next().unwrap();
-    let mut ite_max = ite.clone();
-    while let Some(c) = ite.next() {
-        if let Some(&_n) = ite.peek()
-            && max < c
-        {
-            max = c;
-            ite_max = ite.clone();
-        }
-    }
-    let mut second_max = '0';
-    while let Some(c) = ite_max.next() {
-        if second_max < c {
-            second_max = c;
-        }
-    }
-    let res = format!("{}{}", max, second_max);
-    res.parse().unwrap()
-}
 
 fn part_1_2(line: &Vec<char>, p: usize, n: usize) -> String {
     if n == 0 || p >= line.len() {
         return String::new();
     }
     let (m, new_p) = max_string(line, p, n - 1);
-    let rest = part_2(line, new_p + 1, n - 1);
+    let rest = part_1_2(line, new_p + 1, n - 1);
     format!("{}{}", m, rest)
 }
 
@@ -57,11 +34,14 @@ fn main() {
     let mut res_2 = 0;
     for line in file.lines() {
         let v = string_to_array(&line);
-        let max = part_1(&line); // line.len()>1 ininput
-        let test_str: String = part_2(&v, 0, 12);
-        let test: u128 = test_str.parse().unwrap();
-        res_2 += test;
-        res_1 += max;
+
+        let max_1: String = part_1_2(&v, 0, 2);
+        let max_1: u128 = max_1.parse().unwrap();
+        res_1 += max_1;
+
+        let max_2: String = part_1_2(&v, 0, 12);
+        let max_2: u128 = max_2.parse().unwrap();
+        res_2 += max_2;
     }
     println!("Part 1: {res_1}");
     println!("Part 2: {res_2}");

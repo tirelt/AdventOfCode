94d2a8af3fcf3cc375c40afedf88a0bef5e06bec
Mac theo <theotirel@gmail.com>
2025-12-07 15:39:47 +0800

    2025day07 part2

--- 2025/day07/src/main.rs
+++ 2025/day07/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use std::fs;
 
@@ -5,12 +6,14 @@ fn main() {
     let file = fs::read_to_string("input").unwrap();
     let mut map = Vec::new();
     let mut beams = BTreeSet::new();
+    let mut beams_map = BTreeMap::new();
     let mut j = 0;
     for line in file.lines() {
         let mut v = Vec::new();
         for c in line.chars() {
             if c == 'S' {
                 beams.insert(j);
+                beams_map.insert(j, 1 as u128);
             }
             v.push(c);
             j += 1;
@@ -33,4 +36,21 @@ fn main() {
         beams = new_beams;
     }
     println!("Part 1: {res_1}");
+    for i in 0..(map.len() - 1) {
+        let mut new_beams = BTreeMap::new();
+        for j in beams_map {
+            if map[i + 1][j.0] == '^' {
+                *new_beams.entry(j.0 - 1).or_insert(0) += j.1;
+                *new_beams.entry(j.0 + 1).or_insert(0) += j.1;
+            } else {
+                *new_beams.entry(j.0).or_insert(0) += j.1;
+            }
+        }
+        beams_map = new_beams;
+    }
+    let mut res_2 = 0;
+    for j in beams_map {
+        res_2 += j.1;
+    }
+    println!("Part 2: {res_2}");
 }

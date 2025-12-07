91ee554ed359e198e63950f33ffdbbe4bde1db0e
Mac theo <theotirel@gmail.com>
2025-12-07 14:01:08 +0800

    2025day06 part2

--- 2025/day06/src/main.rs
+++ 2025/day06/src/main.rs
@@ -32,5 +32,39 @@ fn main() {
         }
         res_1 += res;
     }
+    let mut input_vec_2 = Vec::new();
+    let mut res_2 = 0;
+    for line in file.lines() {
+        let mut v = Vec::new();
+        for c in line.chars() {
+            v.push(c);
+        }
+        input_vec_2.push(v);
+    }
+    let mut v = Vec::new();
+    'outer: for j in (0..input_vec_2[0].len()).rev() {
+        let mut number = 0;
+        for i in 0..input_vec_2.len() {
+            let c = input_vec_2[i][j];
+            if c == '*' || c == '+' {
+                v.push(number);
+                if c == '*' {
+                    res_2 += v.iter().fold(1, |x, y| x * y);
+                } else {
+                    res_2 += v.iter().fold(0, |x, y| x + y);
+                }
+                v.clear();
+                continue 'outer;
+            }
+            if c != ' ' {
+                let temp: u128 = c.to_string().parse().unwrap();
+                number = number * 10 + temp;
+            }
+        }
+        if number != 0 {
+            v.push(number);
+        }
+    }
     println!("Part 1: {res_1}");
+    println!("Part 2: {res_2}");
 }

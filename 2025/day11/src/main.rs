f7f24a7530e9f3231a9e35a2ce4afb40257fc420
Mac theo <theotirel@gmail.com>
2025-12-11 18:25:47 +0800

    2025day11 part2

--- 2025/day11/src/main.rs
+++ 2025/day11/src/main.rs
@@ -3,19 +3,26 @@ use std::fs;
 
 fn dfs<'a>(
     front: &'a str,
+    goal: &'a str,
     devices: &'a HashMap<String, Vec<String>>,
     memo: &mut HashMap<&'a str, i64>,
 ) -> i64 {
     if let Some(&v) = memo.get(front) {
         return v;
     }
-    if front == "out" {
+    if front == goal {
         return 1;
+    } else if front == "out" {
+        return 0;
     }
-    let res = devices[front].iter().map(|n| dfs(n, devices, memo)).sum();
+    let res = devices[front]
+        .iter()
+        .map(|n| dfs(n, goal, devices, memo))
+        .sum();
     memo.insert(front, res);
     res
 }
+
 fn main() {
     let file = fs::read_to_string("input").unwrap();
     let mut devices = HashMap::new();
@@ -29,6 +36,16 @@ fn main() {
         }
     }
     let mut memo: HashMap<&str, i64> = HashMap::new();
-    let res_1 = dfs("you", &devices, &mut memo);
+    let res_1 = dfs("svr", "out", &devices, &mut memo);
+
+    //Part 2
+    let path = vec!["svr", "fft", "dac", "out"];
+    let mut res_2 = 1;
+    for i in 0..(path.len() - 1) {
+        memo.clear();
+        let temp = dfs(path[i], path[i + 1], &devices, &mut memo) as i128;
+        res_2 *= temp;
+    }
     println!("Part 1: {res_1}");
+    println!("Part 2: {res_2}");
 }
